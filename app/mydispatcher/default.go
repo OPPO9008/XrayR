70da10ea446db33b0e36c7515a5095796b5d615b
kaban <kaban@galgamer.moe>
2025-02-22 09:05:04 +0800

    API: Add user online stats

--- app/mydispatcher/default.go
+++ app/mydispatcher/default.go
@@ -207,6 +207,18 @@ func (d *DefaultDispatcher) getLink(ctx context.Context, network net.Network, sn
 				}
 			}
 		}
+
+		if p.Stats.UserOnline {
+			name := "user>>>" + user.Email + ">>>online"
+			if om, _ := stats.GetOrRegisterOnlineMap(d.stats, name); om != nil {
+				sessionInbounds := session.InboundFromContext(ctx)
+				userIP := sessionInbounds.Source.Address.String()
+				om.AddIP(userIP)
+				// log Online user with ips
+				// errors.LogDebug(ctx, "user>>>" + user.Email + ">>>online", om.Count(), om.List())
+
+			}
+		}
 	}
 
 	return inboundLink, outboundLink, nil
